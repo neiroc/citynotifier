81343aad52916e92c301731b6c21a4f663d7d6a6
neiroc <neiroc@autistici.org>
2013-11-12 16:09:08 +0100

    Inserito TODO; Login Risolto con JSON;

--- js/login.js
+++ js/login.js
@@ -1,17 +1,26 @@
-					$("#login").submit(function() {
+$(document).ready(function() {
+$("#login").submit(function() {
 
-									var url = "login.php"; // the script where you handle the form input.
+	var url = "/login.php"; // the script where you handle the form input.
+	var credenziali={'username':$("[name='username']").val(),'password':$("[name='password']").val()};
 
-									$.ajax({
-										      type: "POST",
-										      url: url,
-										      data: $("#login").serialize(), // serializes the form's elements.
-										      success: function(data)
-										      {
-										          if(data ==1) window.location="mappa.php"; // show response from the php script
-															else alert(data);			
-										      }
-										    });
-
-									return false; // avoid to execute the actual submit of the form.
+		$.ajax({
+						type: "POST",
+						url: url,
+						data: $("#login").serialize(), // serializes the form's elements.
+						dataType: 'json',
+						success:function(json)
+						{
+							if(json.result=="OK")
+							location.href="mappa.php";
+							else alert(json.result);
+							 		
+						},
+								error: function(e){
+									console.log(e.message);
+							},
 					});
+
+return false; // avoid to execute the actual submit of the form.
+});					
+});
