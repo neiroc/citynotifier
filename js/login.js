abccb12ad8d43b08c24c4b6bd802ea1aad2a4811
boh <boh@boh.(none)>
2013-11-12 16:52:53 +0100

    qualche modifica

--- js/login.js
+++ js/login.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
 $("#login").submit(function() {
 
-	var url = "/login.php"; // the script where you handle the form input.
-	var credenziali={'username':$("[name='username']").val(),'password':$("[name='password']").val()};
+	var url = "login.php"; // the script where you handle the form input.
+
 
 		$.ajax({
 						type: "POST",
@@ -10,11 +10,10 @@ $("#login").submit(function() {
 						data: $("#login").serialize(), // serializes the form's elements.
 						dataType: 'json',
 						success:function(json)
-						{
-							if(json.result=="OK")
+						{	
+							if(json.result=="OK")	
 							location.href="mappa.php";
-							else alert(json.result);
-							 		
+							else alert(json.result); 		
 						},
 								error: function(e){
 									console.log(e.message);
--- login.php
+++ login.php
@@ -1,5 +1,6 @@
  <?php
 
+
 if(isset($_POST['username']) && isset($_POST['password']))	
 {
 	$username = $_POST['username'];
