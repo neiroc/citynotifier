2732a3b2446a755283aa724b37204e7413fc02dc
boh <boh@boh.(none)>
2013-11-12 16:17:38 +0100

    ok Geoloc

--- js/geo.js
+++ js/geo.js
@@ -3,7 +3,7 @@ function geo_and_call() {
 	//get_user_data(false);//prelevo parametri dell'utente da database
 	//$('#bar_username').html("Benvenuto");
 	//set_userdata_function();//setta lo scaricamente della pagina come salvataggio dei dati nel database
-	initialize_base();//crea la mappa con i parametri globali
+	init();//crea la mappa con i parametri globali
 	//gotPosition(pos); // Geolocalizzami
 
 	//initialize();//cambia il centro in base alla geolocalizzazione
@@ -15,16 +15,15 @@ function geo_and_call() {
 
 
 //Inizializza mappa
-function initialize_base() {
-var mapOptions = {
-  center: new google.maps.LatLng(44.496138,11.342325),
-  zoom: 14,
-  mapTypeId: google.maps.MapTypeId.ROADMAP
-};
-var map = new google.maps.Map(document.getElementById("gmap"),
-    mapOptions);
+function init() {
+	var mapOptions = {
+	  center: new google.maps.LatLng(44.496138,11.342325),
+	  zoom: 14,
+	  mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+	var map = new google.maps.Map(document.getElementById("gmap"), mapOptions);
 }
-google.maps.event.addDomListener(window, 'load', initialize);
+google.maps.event.addDomListener(window, 'load', init);
 
 	navigator.geolocation.getCurrentPosition(
 		gotPosition,
