4ed6d2364e369dba822f456075437b7cd7751aee
skunk666 <tiobra666@gmail.com>
2013-12-09 15:23:00 +0100

    login funziona con db server

---
+++ cgi-bin/db_aux.php
@@ -0,0 +1,24 @@
+<?php
+
+
+function connect_db(){
+
+
+    $con=mysqli_connect("localhost","cacaturo","cacaturo123","techweb");
+
+    // Check connection
+    if (mysqli_connect_errno())
+     {
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+      }
+    
+    echo "connected<br>";
+    
+    return $con;
+
+}
+
+
+
+
+?>
