158810b6ee3774ec55121308a309f141eb74bf0b
skunk666 <tiobra666@gmail.com>
2013-12-09 18:25:16 +0100

    aggiunto modulo logout

---
+++ cgi-bin/logout.php
@@ -0,0 +1,10 @@
+ <?php
+ console.log("logout");
+if(isset($_POST[])){
+ 	console.log("logout");
+ 	$result['result'] = "logout effettuato con successo";	
+    $re = json_encode($result);
+	echo $re;
+ 
+} 
+ ?>
