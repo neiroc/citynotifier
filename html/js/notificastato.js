e0451268665049f176f537e3938fedb26248ed4a
skunk666 <tiobra666@gmail.com>
2014-01-21 19:02:55 +0100

    struttura segnalazioni e modifiche stato

---
+++ html/js/notificastato.js
@@ -0,0 +1,54 @@
+$(document).ready(function() {
+	$("#notifica_stato").submit(function() {
+
+
+		var notificaj = {
+
+			event_id = $('#event_id').val(),
+
+			lat = $('#lat').val(),
+
+			lng = $('#lng').val(),
+
+			description = $('#description').val(),
+
+			username = jQuery.cookie('username')
+		
+		}
+
+		var host = "http://"+document.location.hostname ;
+
+		var url = host+"/notifica/" 
+
+		$.ajax({
+
+			url: url, //url a cui fare la chiamata
+
+			async: true, //chiamata asincrona
+
+			type: "POST",// metodo della chiamata
+			
+			contentType: "application/json; charset=utf-8",
+
+			data: JSON.stringify(notificaj), 
+
+			dataType: 'json',
+
+			success:function(call){	
+
+				if(call.result==="nuova segnalazione aperta con successo / segnalazione di un evento già in memoria avvenuta con successo"){	
+
+					alert(call.result);//da finire
+				}
+				else {
+				
+					alert(call.result);
+				} 		
+			},
+			error: function(e){
+				console.log(e.message);
+			},
+		});
+		return false; // avoid to execute the actual submit of the form.
+	});				
+});
