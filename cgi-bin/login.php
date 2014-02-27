753377d6546c042c2191f6735966112d5bb5decf
skunk666 <tiobra666@gmail.com>
2014-02-27 14:55:28 +0100

    fixato segnalazioni e riaggiuti i file

---
+++ cgi-bin/login.php
@@ -0,0 +1,58 @@
+ <?php
+
+require 'db_aux.php';
+
+//recupero il file json contenente le credenziali di login e lo decodifico
+$data = file_get_contents("php://input");
+$login=json_decode($data);
+
+
+//NOTA: CONTROLLARE IL FLUSSO login=json_decode($data,true);
+
+if(($login->{'username'} != Null)&&($login->{'password'} != Null)){
+	
+	$username = $login->{'username'};
+	$password = $login->{'password'};
+
+}
+	
+//connessione con il db
+$con=connect_db();
+
+if($con == False) {
+	$result['result'] = "Errore di connessione al db server";//risposta negativa
+	header('Content-Type: application/json; charset=utf-8');
+	$re = json_encode($result);
+	echo $re;
+}
+	
+//interrogo il db e controllo se i dati di accesso corrispondono		
+$query="SELECT id_utente,username,user_pass,reputation FROM Utenti where username='".$username."' AND user_pass='".$password."';";
+	
+$risposta = mysqli_query($con,$query);
+
+//chiudo la connessione con il db server	
+mysqli_close($con);
+
+if(($row = mysqli_fetch_array($risposta))&&($row['username']==$username)&&($row['user_pass']==$password)){
+						
+	$result['result'] = "login effettuato con successo";//risposta positiva
+	$result['id_utente'] = $row['id_utente'];
+	$result['reputation'] = $row['reputation'];
+	header('Content-Type: application/json; charset=utf-8');
+	$re = json_encode($result);
+	echo $re;
+                           
+}
+
+
+else{
+            
+	$result['result'] = "Credenziali Errate";//risposta negativa
+	header('Content-Type: application/json; charset=utf-8');
+	$re = json_encode($result);
+	echo $re;
+
+}
+
+?> 
---
+++ html/js/notificastato.js
@@ -0,0 +1,69 @@
+$(document).ready(function() {
+	$('#notifystatusbutton').on('click', function() {//da cambiare
+
+
+		var notificaj = {
+
+			event_id : $('#event_id').val(),
+
+			status : $('#status').val,
+
+			lat : $('#lat').val(),
+
+			lng : $('#lng').val(),
+
+			description : $('#notifyDescription').val(),
+
+			username : jQuery.cookie('id_utente')
+		
+		}
+
+		var host = "http://"+document.location.hostname ;
+
+		var url = host+"/notifica/" 
+
+		$.ajax({
+
+			url: url, //url a cui fare la chiamata
+
+			async: true, //chiamata asincrona
+
+			type: "POST",// metodo della chiamata
+			
+			contentType: "application/json; charset=utf-8",
+
+			data: JSON.stringify(notificaj), 
+
+			dataType: 'json',
+
+			success:function(call){	
+
+				if(call.result==="notifica inviata con successo"){	
+
+					if(call.skept){
+
+						successAlert(call.result+" "+call.skept);
+					
+					}
+					else{
+					
+						successAlert(call.result);
+					
+					}
+				}
+				else {
+				
+					errorAlert(call.result+": "+call.errore);
+				} 		
+			},
+			error: function(e){
+				errorAlert("errore di risposta dal server");
+			},
+		});
+		return false; // avoid to execute the actual submit of the form.
+	});				
+});
+
+
+
+
